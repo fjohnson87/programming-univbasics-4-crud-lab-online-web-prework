25427af35cb22bba4864f5b6605dac6834c1e3f2
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-04 01:31:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 def add_element_to_start_of_array(array, element)
   famous_characters = ["Thor", "Hulk", "Loki"]
  
-famous_characters .unshift ("Spiderman")
+famous_characters.unshift ("Spiderman")
 end
 
 def remove_element_from_end_of_array(array)
